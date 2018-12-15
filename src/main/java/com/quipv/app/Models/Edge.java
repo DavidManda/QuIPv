94273f39a60c3216f5a7244ccce81fab40cf5b63
DavidManda <davidmanda1998@gmail.com>
2018-12-15 21:55:44 +0000

    Added equals and hashCode methods on Edge

--- src/main/java/com/quipv/app/Models/Edge.java
+++ src/main/java/com/quipv/app/Models/Edge.java
@@ -1,5 +1,7 @@
 package com.quipv.app.Models;
 
+import java.util.Objects;
+
 public class Edge {
     private GraphNode origin;
     private GraphNode destination;
@@ -21,5 +23,30 @@ public class Edge {
 
     public Integer getWeight() {
         return weight;
+    }
+
+    public void incrementWeight(Integer weight){
+        this.weight += weight;
+    }
+
+    @Override
+    public boolean equals(Object o){
+
+        if(o == this){
+            return true;
+        }
+
+        if(!(o instanceof Edge)){
+            return false;
+        }
+
+        Edge edge = (Edge) o;
+
+        return this.origin == edge.origin && this.destination == edge.destination;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.origin.hashCode(), this.destination.hashCode());
     }
 }
