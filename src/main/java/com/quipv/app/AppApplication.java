c7e745e956cf2e6f454a46ccb9a29ab6d9670ef1
DavidManda <davidmanda1998@gmail.com>
2019-02-12 15:18:54 +0000

    Created default properties file

--- src/main/java/com/quipv/app/AppApplication.java
+++ src/main/java/com/quipv/app/AppApplication.java
@@ -2,7 +2,13 @@ package com.quipv.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
+@PropertySources({
+		@PropertySource(value = "classpath:application.properties"),
+		@PropertySource(value = "classpath:application.default.properties")
+})
 @SpringBootApplication
 public class AppApplication {
 
