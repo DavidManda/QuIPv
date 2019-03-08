fa3a5e4aa96b360bdaa0358972bf4b8370092936
DavidManda <davidmanda1998@gmail.com>
2019-03-08 20:53:52 +0000

    Sterted work on login using database users

--- src/main/java/com/quipv/app/Config/WebSecurityConfig.java
+++ src/main/java/com/quipv/app/Config/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.quipv.app.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,9 +12,25 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Autowired
+//    DataSource dataSource;
+//
+//    @Autowired
+//    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+//
+//        auth.jdbcAuthentication().dataSource(dataSource)
+//                .usersByUsernameQuery(
+//                        "select username,password, enabled from users where username=?")
+//                .authoritiesByUsernameQuery(
+//                        "select username, role from user_roles where username=?");
+//    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -22,6 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .and()
                 .formLogin()
                     .loginPage("/login")
+                    .usernameParameter("username").passwordParameter("password")
                     .permitAll()
                     .and()
                 .logout()
