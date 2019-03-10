680f62435d24e44e24a5b36c0a16360eaf927eb1
DavidManda <davidmanda1998@gmail.com>
2019-03-10 14:15:09 +0000

    Removed application.default.properties, moved everything in application.properties

--- src/main/java/com/quipv/app/AppApplication.java
+++ src/main/java/com/quipv/app/AppApplication.java
@@ -2,13 +2,7 @@ package com.quipv.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
-@PropertySources({
-		@PropertySource(value = "classpath:application.properties"),
-		@PropertySource(value = "classpath:application.default.properties")
-})
 @SpringBootApplication
 public class AppApplication {
 	public static void main(String[] args)
