aaf4b1b89553e97ee91d6a1dcd85048d42285e4e
DavidManda <davidmanda1998@gmail.com>
2019-03-10 14:40:46 +0000

    Annotated repositories with @Component and implemented UserDetailsService

--- src/main/java/com/quipv/app/Repositories/RoleRepository.java
+++ src/main/java/com/quipv/app/Repositories/RoleRepository.java
@@ -2,6 +2,10 @@ package com.quipv.app.Repositories;
 
 import com.quipv.app.Models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+@Component
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long>{
 }
--- src/main/java/com/quipv/app/Repositories/UserRepository.java
+++ src/main/java/com/quipv/app/Repositories/UserRepository.java
@@ -2,7 +2,11 @@ package com.quipv.app.Repositories;
 
 import com.quipv.app.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+@Component
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
